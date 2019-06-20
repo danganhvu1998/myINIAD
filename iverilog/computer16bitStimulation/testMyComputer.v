afe24e715ba6f49f93c0819a06d0914a79961b49
Dang Anh Vu <danganhvu1998@gmail.com>
2019-06-20 16:07:39 +0900

    holy. I built a computer

--- iverilog/computer16bitStimulation/testMyComputer.v
+++ iverilog/computer16bitStimulation/testMyComputer.v
@@ -2,23 +2,28 @@ module test_My_ALU();
     reg [15:0] A, B, IR;
     wire IsALU, S_SUB, S_FAS, S_AND, S_OR, S_XOR, S_NOT;
     wire [15:0] R;
-
-    my_decoder decoder1(IR, IsALU, S_SUB, S_FAS, S_AND, S_OR, S_XOR, S_NOT);
-    my_ALU alu1( A, B, R, S_SUB, S_FAS, S_AND, S_OR, S_XOR, S_NOT);
+    my_CPU16v0 cpu16v0(IR);
 
     initial begin
-        $monitor( $stime, "IR=%b A=%b B=%b R=%b S_SUB=%d S_FAS=%d S_AND=%d S_OR=%d S_XOR=%d S_NOT=%d", IR, A, B, R, S_SUB, S_FAS, S_AND, S_OR, S_XOR, S_NOT );
-        IR = 16'b0000_xxxx_1010_xxxx;
-        A <= 65280; B <= 257; 
+        $monitor( $stime, " CK=%b PK=%d IR=%b A=%b B=%b R=%b Cout=%d S_SUB=%d S_FAS=%d S_AND=%d S_OR=%d S_XOR=%d S_NOT", 
+            CK, cpu16v1.PC, cpu16v0.IR,cpu16v0.A, cpu16v0.B, cpu16v0.R, cpu16v0.Cout, 
+            cpu16v0.S_SUB, cpu16v0.S_FAS, cpu16v0.IsAND, cpu16v0.IsOR, cpu16v0.IsXOR, cpu16v0.IsNOT );
+        cpu16v0.register1.registers[0] = 65280;
+        cpu16v0.register1.registers[1] = 257;
+
+        IR <= 16'b0000_0000_1010_0001; 
         #10
-        IR = 16'b0000_xxxx_0010_xxxx;
-        A <= 16; B <= 9; 
+        cpu16v0.register1.registers[0] = 16;
+        cpu16v0.register1.registers[1] = 9;
+        IR <= 16'b0000_0000_0010_0001;
         #10
-        IR = 16'b0000_xxxx_1100_xxxx;
-        A <= 65280; B <= 255; 
+        cpu16v0.register1.registers[0] = 65280;
+        cpu16v0.register1.registers[1] = 257;
+        IR <= 16'b0000_0000_1100_0001;
         #10
-        IR = 16'b0000_xxxx_1110_xxxx;
-        A <= 43520; B <= 21760;
+        cpu16v0.register1.registers[0] = 43520;
+        cpu16v0.register1.registers[1] = 21760;
+        IR <= 16'b0000_0000_1110_0001;
         $finish;
     end
 endmodule
---
+++ iverilog/computer16bitStimulation/test_releaseTheKrakon.v
@@ -0,0 +1,45 @@
+module test_my_cpu16;
+reg CK;
+my_CPU16v1 cpu16v1(CK);
+
+always #10 begin
+    CK <= ~CK;
+end
+
+initial begin
+    $monitor( $stime, " PC=%d IR=%b A=%b B=%b R=%b Cout=%d S_SUB=%d S_FAS=%d S_AND=%d S_OR=%d S_XOR=%d S_NOT=%d", 
+              cpu16v1.PC, cpu16v1.IR, cpu16v1.A, cpu16v1.B, cpu16v1.R, cpu16v1.Cout, 
+              cpu16v1.S_SUB, cpu16v1.S_FAS, cpu16v1.IsAND, cpu16v1.IsOR, cpu16v1.IsXOR, cpu16v1.IsNOT );
+
+    /// memory
+    // ADD R0, R1
+    CK=0;
+    #10
+    cpu16v1.memory1.mem[0] = 8'b0000_0000;
+    cpu16v1.memory1.mem[1] = 8'b1010_0001;
+    
+    // SUB R2, R3
+    cpu16v1.memory1.mem[2] = 8'b0000_0010;
+    cpu16v1.memory1.mem[3] = 8'b0010_0011;
+
+    // AND R0, R4
+	cpu16v1.memory1.mem[4] = 8'b0000_0001;
+	cpu16v1.memory1.mem[5] = 8'b1100_0100;
+    // OR R5, R6
+    cpu16v1.memory1.mem[6] = 8'b0000_0101;
+	cpu16v1.memory1.mem[7] = 8'b1110_0110;  
+    // register
+    cpu16v1.register1.registers[0] = 65280;
+    cpu16v1.register1.registers[1]= 257;
+	cpu16v1.register1.registers[2]= 16;
+	cpu16v1.register1.registers[3]= 9;
+	cpu16v1.register1.registers[4]= 255;
+	cpu16v1.register1.registers[5]= 43520;
+	cpu16v1.register1.registers[6]= 21760;
+    //<Please fill in here>   
+    // add clock
+    //<Please fill in here>   
+    #50
+   $finish;
+end
+endmodule
