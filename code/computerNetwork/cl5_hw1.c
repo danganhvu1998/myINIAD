58891500becf3e1bcdf2b20c85cfa7adb5b75867
Dang Anh Vu <danganhvu1998@gmail.com>
2019-10-22 16:16:17 +0900

    doing hard hw

--- code/computerNetwork/cl5_hw1.c
+++ code/computerNetwork/cl5_hw1.c
@@ -2,11 +2,13 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <string.h>
 
 const int BUF_SIZE = 256;
-const char SERVER_IP[] = "127.0.0.1";
-const unsigned short SERVER_PORTNUM = 9000;
-const char MESSAGE[] = "Hello echo server.";
+char SERVER_IP[100];
+unsigned short SERVER_PORTNUM = 8000;
+char MESSAGE[1000];
+const char END_MESSAGE[] = "\n\n";
 
 void error_handler(const char *message){
     fprintf(stderr,"%s",message);
@@ -15,51 +17,52 @@ void error_handler(const char *message){
 
 int main(int argc, char *argv[]){
     int sock;
-    //create a socket
-    if ((sock = socket(PF_INET, SOCK_STREAM,IPPROTO_TCP)) < 0){
-        error_handler("socket() failed.\n");
+    while(1){
+      printf("SERVER IP: ");
+      fgets(SERVER_IP, sizeof(SERVER_IP), stdin);
+      printf("SERVER IP: ");
+      scanf("%hd", &SERVER_PORTNUM);
+      printf("REQUEST: ");
+      fgets(MESSAGE, sizeof(MESSAGE), stdin);
+      strcpy(MESSAGE, strcat(MESSAGE, END_MESSAGE));
+      if ((sock = socket(PF_INET, SOCK_STREAM,IPPROTO_TCP)) < 0){
+          error_handler("socket() failed.\n");
+      }
+
+      struct sockaddr_in server_addr;
+      memset(&server_addr,0,sizeof(server_addr));
+      server_addr.sin_family = AF_INET;
+      server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+      server_addr.sin_port = htons(SERVER_PORTNUM);
+      if(connect(sock,(struct sockaddr *) &server_addr,sizeof(server_addr)) < 0){
+          error_handler("connect() failed.\n");
+      }
+      int message_size = strlen(MESSAGE);
+      int total_sent_size;
+      for (total_sent_size = 0; total_sent_size < message_size; ){
+          int sent_size;
+          if ((sent_size = send(sock,&MESSAGE[total_sent_size],message_size-total_sent_size,0)) < 0){
+              error_handler("send() failed.\n");
+          }
+          total_sent_size += sent_size;
+      }   
+      char buf[BUF_SIZE];
+      int received__bytes = 0, total_received_bytes = 0;
+      printf("Received messages: ");
+      while (1)
+      {
+          // receive messages from the server
+          if((received__bytes = recv(sock,buf,sizeof(buf)-1,0)) < 0){
+              error_handler("recv() failed.\n");
+          }
+          else if (received__bytes == 0) {break; }
+          total_received_bytes += received__bytes;
+          buf[received__bytes] = '\0';
+          printf("%s", buf);
+      }
+      printf("\n");
+      close(sock);
+      //close the socket
     }
-
-    //initialize
-    struct sockaddr_in server_addr;
-    memset(&server_addr,0,sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_addr.sin_port = htons(SERVER_PORTNUM);
-
-    //connect to the server
-    if(connect(sock,(struct sockaddr *) &server_addr,sizeof(server_addr)) < 0){
-        error_handler("connect() failed.\n");
-    }
-
-    //send a message to the server
-    int message_size = strlen(MESSAGE);
-    int total_sent_size;
-    for (total_sent_size = 0; total_sent_size < message_size; ){
-        int sent_size;
-        if ((sent_size = send(sock,&MESSAGE[total_sent_size],message_size-total_sent_size,0)) < 0){
-            error_handler("send() failed.\n");
-        }
-        total_sent_size += sent_size;
-    }   
-    char buf[BUF_SIZE];
-    int received__bytes = 0, total_received_bytes = 0;
-    printf("Received messages: ");
-    while (1)
-    {
-        // receive messages from the server
-        if((received__bytes = recv(sock,buf,sizeof(buf)-1,0)) < 0){
-            error_handler("recv() failed.\n");
-        }
-        else if (received__bytes == 0) {break; }
-        total_received_bytes += received__bytes;
-        buf[received__bytes] = '\0';
-        printf("%s",buf);
-    }
-    printf("\n");
-
-    //close the socket
-    close(sock);
-    exit(0);
-    
+    exit(0); 
 }
