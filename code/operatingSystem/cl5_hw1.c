ea1a6516c24000250dbdc7165176ae7004d044ad
Dang Anh Vu <danganhvu1998@gmail.com>
2019-10-24 15:55:36 +0900

    doing homework

---
+++ code/operatingSystem/cl5_hw1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+  unsigned char buf[1];
+  int fr, fw, rlen, wlen;
+
+  fr = open(argv[1], O_RDONLY);
+  fw = open(argv[2], O_WRONLY);
+
+  for(;;){
+    rlen = read(fr, buf, sizeof(buf));
+    if(rlen<0){
+      return 1;
+    } else if(rlen==0) {
+      return 0;
+    } else {
+      for(int j=0; j<rlen;){
+        wlen = write(fw, buf+j, rlen-j);
+        if(wlen<0) return 1;
+        j += wlen;
+      }
+    }
+  }
+  close(fr);
+  close(fw);
+  return 0;
+}
