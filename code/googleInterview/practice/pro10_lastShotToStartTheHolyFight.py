afad8f633ecb73e2c161ac49652a7a637fa6ee32
Dang Anh Vu <danganhvu1998@gmail.com>
2019-11-08 22:55:56 +0900

    what should I do now?

--- code/googleInterview/practice/pro10_lastShotToStartTheHolyFight.py
+++ code/googleInterview/practice/pro10_lastShotToStartTheHolyFight.py
@@ -1,3 +1,7 @@
+# Given a string with a certain rule: k[string] should be expanded to string k times. 
+# So for example, 3[abc] should be expanded to abcabcabc. 
+# Nested expansions can happen, so 2[a2[b]c] should be expanded to abbcabbc.
+
 import re
 
 def extrac(s):
