0739fdb6f2f9b7040512cc2cd3facd7fe6127272
Dang Anh Vu <danganhvu1998@gmail.com>
2019-11-21 23:18:46 +0900

    finished optional hw: shell with |

--- code/operatingSystem/cl6_fork_pipe/myshell_optional.c
+++ code/operatingSystem/cl6_fork_pipe/myshell_optional.c
@@ -39,10 +39,10 @@ void buf_to_cmd_string(){
   token = strtok(NULL, ">");
   write_to_file_name = token;
   if(write_to_file_name == NULL){
-    printf("not write to file\n");
+    // printf("not write to file\n");
   } else {
     start_end_space_remover(write_to_file_name);
-    printf("write to \"%s\"\n", write_to_file_name);
+    // printf("write to \"%s\"\n", write_to_file_name);
   }
   // Split cmd_strings into many cmd_string
   start_end_space_remover(cmd_strings);
@@ -62,11 +62,11 @@ void string_to_token(int curr_cmd){
   char BUF[500];
   strcpy(BUF, cmd_string[curr_cmd]);
   curr_cmd_index = 0;
-  //printf("%s\n", BUF);
+  //printf("--%s--\n", BUF);
   token = strtok(BUF, " ");
   while( token != NULL ) {
     cmd[curr_cmd_index] = token;
-    //printf("--> %s\n", cmd[curr_cmd_index]);
+    //printf("--> -%s-\n", cmd[curr_cmd_index]);
     ++curr_cmd_index;
     token = strtok(NULL, " ");
   }
@@ -74,11 +74,11 @@ void string_to_token(int curr_cmd){
 }
 
 int main(){
-  char curr_result[8192] = "HAHA\nHaha\n";
+  char curr_result[8192] = "";
   for(;;){
     printf("\n> ");
     fgets(buf, sizeof(buf), stdin);
-    printf("BUF: %s\n", buf);
+    // printf("BUF: %s\n", buf);
     buf_to_cmd_string();
     int curr_cmd = 0;
     while(cmd_string[curr_cmd] != NULL){
@@ -95,8 +95,8 @@ int main(){
           fprintf(stderr, "Pipe failed\n");
           return -1;
       }
-      printf("CURRENT CMD: %d : %s\n", curr_cmd, cmd_string[curr_cmd]);
-      printf("RESULT:\n----------------------------------------------------------------\n");
+      // printf("CURRENT CMD: %d : %s\n", curr_cmd, cmd_string[curr_cmd]);
+      // printf("RESULT:\n----------------------------------------------------------------\n");
       pid = fork();
       if (pid < 0) {
           fprintf(stderr, "Fork failed\n");
@@ -104,35 +104,55 @@ int main(){
       }
       if(pid > 0){ // Parent
         /* Parent */
-        close(pipefd2[0]);
-        write(pipefd2[1], curr_result, strlen(curr_result)+1);
+        close(pipefd[0]);
+        write(pipefd[1], curr_result, strlen(curr_result));
         close(pipefd[1]);
-        
+
+        close(pipefd2[1]);
         char ch;
-        //waitpid(pid, &ret, 0);
-        while(read(pipefd[0], &ch, 1) > 0)
+        waitpid(pid, &ret, 0);
+        while(read(pipefd2[0], &ch, 1) > 0)
         {
           curr_result[curr_result_wr] = ch;
           ++curr_result_wr;
         }
         curr_result[curr_result_wr] = '\0';
-        printf("%s----------\nResult Length: %ld\n\n----------------------------------------------------------------\n\n", curr_result, strlen(curr_result));
+        ++curr_cmd;
       } else { 
         /* Child */
-        printf("THis is child: %s\n-----\n", curr_result);
-        close(pipefd[0]);
-        close(STDOUT_FILENO);
-        dup2(pipefd[1], STDOUT_FILENO);
-        
-        close(pipefd2[1]);
+        // printf("This is child: \n%s\nNext CMD: %s\n-----\n", curr_result, cmd_string[curr_cmd]);
+        close(pipefd[1]);
         close(STDIN_FILENO);
-        dup2(pipefd2[0], STDIN_FILENO);
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
         
-        string_to_token(curr_cmd);
-        execvp(cmd[0], cmd);
-        return 0;
+        close(pipefd2[0]);
+        close(STDOUT_FILENO);
+        dup2(pipefd2[1], STDOUT_FILENO);
+        
+        char* token;
+        char* cmdd[512];
+        char BUF[500];
+        strcpy(BUF, cmd_string[curr_cmd]);
+        int curr_cmd_index = 0;
+        //printf("--%s--\n", BUF);
+        token = strtok(BUF, " ");
+        while( token != NULL ) {
+          cmdd[curr_cmd_index] = token;
+          //printf("--> -%s-\n", cmd[curr_cmd_index]);
+          ++curr_cmd_index;
+          token = strtok(NULL, " ");
+        }
+        cmdd[curr_cmd_index] = NULL;
+        execvp(cmdd[0], cmdd);
       }
-      ++curr_cmd;
+    }
+    if(write_to_file_name==NULL) {
+      printf("%s\n", curr_result);
+    } else {
+      FILE *file = fopen(write_to_file_name, "w");
+      fwrite(&curr_result, strlen(curr_result), 1, file);
+      fclose(file);
     }
   }
 }
