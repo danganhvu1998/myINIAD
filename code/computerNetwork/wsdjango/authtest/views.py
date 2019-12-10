ceab0b8c6c833df753d4271f7f8f173978948d79
Dang Anh Vu <danganhvu1998@gmail.com>
2019-12-10 16:16:01 +0900

    done :(((

--- code/computerNetwork/wsdjango/authtest/views.py
+++ code/computerNetwork/wsdjango/authtest/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.admin import login_required
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
