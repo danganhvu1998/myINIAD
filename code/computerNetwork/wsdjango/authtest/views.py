3dd7c7d590277783837b3c3bfd22c08a0bf52f62
Dang Anh Vu <danganhvu1998@gmail.com>
2019-12-10 16:14:43 +0900

    add private page

--- code/computerNetwork/wsdjango/authtest/views.py
+++ code/computerNetwork/wsdjango/authtest/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.admin import login_required
 
 # Create your views here.
 def home(request):
   return render(request, 'authtest/home.html', {})
 
+@login_required
 def private_page(request):
   return render(request, 'authtest/private.html', {})
 
