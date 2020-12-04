daccaba7315ad29d8588d9054279605ffd6e5b6c
Anh Vu <danganhvu1998@gmail.com>
2020-12-04 10:42:52 +0900

    openCV

---
+++ code/openCV/graphToNumber.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(
+                        imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(
+                        imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(
+                    imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(
+                    imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+def applyHSVFilter(img, h_min, h_max, s_min, s_max, v_min, v_max):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    return mask
+
+
+# Setting to get blue line 0 179 57 255 170 236
+# Setting to get x and y bar 0 0 0 255 68 126
+
+img = cv2.imread('./resources/graph.png')
+graphBarImg = applyHSVFilter(img, 0, 0, 0, 255, 68, 126)
+lineImg = applyHSVFilter(img, 51, 111, 135, 255, 105, 216)
+imgStack = stackImages(1, ([img, graphBarImg, lineImg]))
+cv2.imshow("Stacked Images", imgStack)
+cv2.waitKey(20000)
