8896d49af4cbff4020287c15e880d79058297792
danganhvu <danganhvu1998@gmail.com>
2021-10-08 10:49:34 +0900

    whatever

--- code/PythonINIAD/5And7.py
+++ code/PythonINIAD/5And7.py
@@ -1,11 +1,14 @@
-def sevenNotFive():
-	result = []
-	for i in range(105, 201, 7):
-		print(i)
-		if(i%5==0): continue
-		result.append(i)
-	return result
+import numpy as np
 
-result = sevenNotFive()
-for i in result:
-	print(str(i)+"-", end="")
+a = np.array( [ 1, 2, 3] )
+print( a.shape )
+print( a )
+a = a.reshape( -1, 1 ) 
+print( a.shape )
+print( a )
+b = np.array( [ [ 1, 2, 3 ], [ 4, 5, 6 ] ] )
+print( b.shape )
+print( b )
+b = b.reshape( -1, 3, 1 ) 
+print( b.shape )
+print( b )
